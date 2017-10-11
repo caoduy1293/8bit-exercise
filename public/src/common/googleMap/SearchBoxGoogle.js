3e0abf9631c23b396d0a6d950cbdb448f43e2292
Duy caoquang <caoduy1293@gmail.com>
2017-10-11 11:17:47 +0700

    fix google map

--- public/src/common/googleMap/SearchBoxGoogle.js
+++ public/src/common/googleMap/SearchBoxGoogle.js
@@ -2,107 +2,72 @@
  * Created by caoquang on 10/10/2017.
  */
 import React, {Component} from "react"
-import { compose, withProps } from "recompose"
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
-import SearchBox from "react-google-maps/lib/components/places/SearchBox";
+import { compose, withProps, lifecycle } from "recompose";
+import {
+    withScriptjs,
+} from "react-google-maps";
+import StandaloneSearchBox from "react-google-maps/lib/components/places/StandaloneSearchBox";
 
-const MyMapComponent = compose(
+
+const PlacesWithStandaloneSearchBox = compose(
     withProps({
         googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyD0UDOSXZ0ljbOgf62pW8oOuKXt5dwjm-k&v=3.exp&libraries=geometry,drawing,places",
         loadingElement: <div style={{ height: `100%` }} />,
-        containerElement: <div style={{ height: `400px` }} />,
-        mapElement: <div style={{ height: `100%` }} />,
+        containerElement: <div style={{ height: `200px`, marginBottom: `50px` }} />,
     }),
-    withScriptjs,
-    withGoogleMap
-)((props) =>
-<GoogleMap
-    ref={props.onMapMounted}
-    defaultZoom={15}
-    center={props.center}
-    onBoundsChanged={props.onBoundsChanged}>
-    <SearchBox
-        ref={props.onSearchBoxMounted}
-        bounds={props.bounds}
-        controlPosition={google.maps.ControlPosition.TOP_LEFT}
-        onPlacesChanged={props.onPlacesChanged}>
-        <input
-            type="text"
-            placeholder="Add your address"
-            style={{
-                boxSizing: `border-box`,
-                border: `1px solid transparent`,
-                width: `240px`,
-                height: `32px`,
-                marginTop: `27px`,
-                padding: `0 12px`,
-                borderRadius: `3px`,
-                boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-                fontSize: `14px`,
-                outline: `none`,
-                textOverflow: `ellipses`,
-            }}
-        />
-    </SearchBox>
-    {props.markers.map((marker, index) =>
-        <Marker key={index} position={marker.position} />
-    )}
-</GoogleMap>
+    withScriptjs
+)(props =>
+    <div data-standalone-searchbox="" style={{ marginBottom: `50px` }}>
+        <StandaloneSearchBox
+            ref={props.onSearchBoxMounted}
+            bounds={props.bounds}
+            onPlacesChanged={props.onPlacesChanged}
+        >
+            <input
+                type="text"
+                placeholder="choose your address"
+                style={{
+                    boxSizing: `border-box`,
+                    border: `1px solid transparent`,
+                    width: `240px`,
+                    height: `32px`,
+                    padding: `0 12px`,
+                    borderRadius: `3px`,
+                    boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                    fontSize: `14px`,
+                    outline: `none`,
+                    textOverflow: `ellipses`,
+                }}
+            />
+        </StandaloneSearchBox>
+
+    </div>
 );
+
 
 class MyFancyComponent extends Component {
 
     componentWillMount() {
-        const refs = {};
+        const refs = {}
+
         this.setState({
-            bounds: null,
-            center: {
-                lat: 41.9, lng: -87.624
-            },
-            markers: [],
-            onMapMounted: ref => {
-                refs.map = ref;
-            },
-
-            onBoundsChanged: () => {
-                this.setState({
-                    bounds: refs.map.getBounds(),
-                    center: refs.map.getCenter(),
-                })
-            },
-
+            places: [],
             onSearchBoxMounted: ref => {
                 refs.searchBox = ref;
             },
-
             onPlacesChanged: () => {
                 const places = refs.searchBox.getPlaces();
-                const bounds = new google.maps.LatLngBounds();
                 this.props.onPlacesChanged(places[0]);
-                places.forEach(place => {
-                    if (place.geometry.viewport) {
-                        bounds.union(place.geometry.viewport)
-                    } else {
-                        bounds.extend(place.geometry.location)
-                    }
-                });
-                const nextMarkers = places.map(place => ({
-                    position: place.geometry.location,
-                }));
-                const nextCenter = _.get(nextMarkers, '0.position', this.state.center);
-
                 this.setState({
-                    center: nextCenter,
-                    markers: nextMarkers,
+                    places,
                 });
-                // refs.map.fitBounds(bounds);
-            }
-        });
+            },
+        })
     }
 
     render() {
         return (
-            <MyMapComponent {...this.state} />
+            <PlacesWithStandaloneSearchBox {...this.state} />
         )
     }
 }
